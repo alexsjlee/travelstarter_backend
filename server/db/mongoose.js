663d532583328940d2f7dc669aa35f3843b774ea
alexsjlee <sjlee89@gmail.com>
2017-07-11 12:37:54 -0700

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/travelstarter');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/travelstarter');
 
 module.exports = {
     mongoose
